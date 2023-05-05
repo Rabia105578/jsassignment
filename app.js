c6c562268147289c43d1072fc300684973077be9
Rabia105578 <132687493+Rabia105578@users.noreply.github.com>
2023-05-05 20:19:09 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-// Questions1
-var myName="Rabia"
-alert(myName);
-// Questions2
-var titleMsg="Hello everybody"
-alert(titleMsg);
-// Question3
-var myName="Rabia"
-var myAge="16"
-var myQualification="Intermidiate"
-
-alert(myName);
-alert(myAge);
-alert(myQualification);
-// Question4
-alert("Pizza\nPizz\nPiz\nPi\nP");
-// Question5
-var myEmail="My Email adress is rabiakhan486@gmail.com"
-alert(myEmail);
-// Question6
-var myBook="Iam trying to learnfrom the book A smarter way to learn Javascript "
-alert(myBook);
-//Question7
-var pattern="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-alert(pattern);
+alert("helloworld")
+alert("Error!please enter a valid passward")
+alert("Wellcome to Js land Happy coding")
+alert("Wellcome to Js land")
+alert("Happy coding")
+alert("Hello...Ican run Js through my web browser's console")
